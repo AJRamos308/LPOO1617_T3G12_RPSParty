11b54d8130a27e39a18d9a043e49a81cb7796d4a
Beatriz Baldaia <bibibaldaia@hotmail.com>
2017-05-04 18:41:07 +0100

    estou a mexer na classe MatchStage

--- core/src/com/rpsparty/game/view/GameScreen.java
+++ core/src/com/rpsparty/game/view/GameScreen.java
@@ -1,10 +1,155 @@
 package com.rpsparty.game.view;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.Net;
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.net.ServerSocket;
+import com.badlogic.gdx.net.ServerSocketHints;
+import com.badlogic.gdx.net.Socket;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.TextField;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.rpsparty.game.RPSParty;
+import com.rpsparty.game.view.entities.HelpButton;
+import com.rpsparty.game.view.entities.MatchStage;
+
+import java.net.Inet4Address;
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
 
 /**
  * Created by bibib on 04/05/2017.
  */
 
 public class GameScreen extends ScreenAdapter {
+    public final static float PIXEL_TO_METER = 0.04f;
+
+    private final RPSParty game;
+    private MatchStage matchStage;
+    /**
+     * The camera used to show the viewport.
+     */
+    private final OrthographicCamera camera;
+    /**
+     * The width of the viewport in meters. The height is
+     * automatically calculated using the screen ratio.
+     */
+    private static final float VIEWPORT_WIDTH = 20;
+    private HelpButton helpButton;
+
+
+
+    public GameScreen(RPSParty game) {
+        this.game = game;
+        matchStage = new MatchStage(RPSParty game);
+        loadAssets();
+        camera = createCamera();
+    }
+    /**
+     * Loads the assets needed by this screen.
+     */
+    private void loadAssets() {
+        this.game.getAssetManager().load( "badlogic.jpg" , Texture.class);
+        this.game.getAssetManager().finishLoading();
+    }
+    /**
+     * Creates the camera used to show the viewport.
+     *
+     * @return the camera
+     */
+    private OrthographicCamera createCamera() {
+        float ratio = ((float) Gdx.graphics.getHeight() / (float)Gdx.graphics.getWidth());
+        OrthographicCamera camera = new OrthographicCamera(VIEWPORT_WIDTH / PIXEL_TO_METER, VIEWPORT_WIDTH / PIXEL_TO_METER * ratio);
+
+        camera.position.set(camera.viewportWidth / 2f, camera.viewportHeight / 2f, 0);
+        camera.update();
+
+        return camera;
+    }
+
+    /**
+     * Renders this screen.
+     *
+     * @param delta time since last renders in seconds.
+     */
+    @Override
+    public void render(float delta) {
+        //Gdx.gl.glClearColor( 103/255f, 69/255f, 117/255f, 1 );
+        Gdx.gl.glClearColor( 1, 1, 1, 1 );
+        Gdx.gl.glClear( GL20.GL_COLOR_BUFFER_BIT | GL20.GL_DEPTH_BUFFER_BIT );
+        camera.update();
+        game.getBatch().begin();
+        game.getBatch().setProjectionMatrix(camera.combined);
+        game.getBatch().end();
+        if (Gdx.input.isKeyPressed(Input.Keys.BACK)) {
+            game.backpressed = true;
+            game.setScreen(new MainMenuScreen(game));
+            Gdx.input.setCatchBackKey(true);
+        }
+    }
+
+    public void addButtons() {
+        helpButton = new HelpButton(game);
+    }
+    //TODO: Back Button
+    public void addListeners() {
+        helpButton.addListener(new ClickListener() {
+            public void clicked(InputEvent event, float x, float y) {
+                //TODO: fazer setScreen()
+                System.out.println("HELP!");
+            }});
+    }
+    /*
+    *espera que um cliente se ligue ao nosso socket
+    * e le o que o cliente escreve para o socket
+     */
+    public void createThread() {
+        // Now we create a thread that will listen for incoming socket connections
+        new Thread(new Runnable(){
+
+            @Override
+            public void run() {
+                ServerSocketHints serverSocketHint = new ServerSocketHints();
+                // 0 means no timeout.  Probably not the greatest idea in production!
+                serverSocketHint.acceptTimeout = 0;
+
+                // Create the socket server using TCP protocol and listening on 9021
+                // Only one app can listen to a port at a time, keep in mind many ports are reserved
+                // especially in the lower numbers ( like 21, 80, etc )
+                ServerSocket serverSocket = Gdx.net.newServerSocket(Net.Protocol.TCP, 9021, serverSocketHint);
+
+                Socket socket = serverSocket.accept(null);//fica a espera que alguem se conecte
+                System.out.println("aceitou cliente");
+                game.setScreen(new MainMenuScreen(game));
+                /*
+                // Loop forever
+                while(true){
+                    // Create a socket; o accept bloqueia/nao se avanca no código enquanto um cliente nao se ligar ao nosso socket
+                    Socket socket = serverSocket.accept(null);
+                    //TODO: mudar de ecra mal o cliente se ligue
+                    // Read data from the socket into a BufferedReader
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+
+                    try {
+                        // Read to the next newline (\n) and display that text on labelMessage
+                        System.out.println(buffer.readLine());
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+                */
+            }
+        }).start(); // And, start the thread running
+    }
 }
--- core/src/com/rpsparty/game/view/entities/MatchStage.java
+++ core/src/com/rpsparty/game/view/entities/MatchStage.java
@@ -1,12 +1,49 @@
 package com.rpsparty.game.view.entities;
 
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.World;
+import com.badlogic.gdx.scenes.scene2d.InputListener;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.rpsparty.game.RPSParty;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
 
 public class MatchStage extends Stage {
     private World world;
 
-    //private final RockHandActor rock;
-    //private final PaperHandActor paper;
-    //private final ScissorHandActor scissor;
+    private final RockHandActor rock;
+    private final PaperHandActor paper;
+    private final ScissorHandActor scissor;
+
+    MatchStage (RPSParty game) {
+        world = new World(new Vector2(0,0), true);
+        rock = new RockHandActor(game);
+        paper = new PaperHandActor(game);
+        scissor = new ScissorHandActor(game);
+        rock.setPosition(50,0);
+        paper.setPosition(100,0);
+        scissor.setPosition(200,0);
+        addListenersActors();
+        addActor(rock);
+        addActor(paper);
+        addActor(scissor);
+    }
+
+
+    public void loadAssets(RPSParty game) {
+        game.getAssetManager().load( "badlogic.jpg" , Texture.class);
+        game.getAssetManager().load( "test.jpg" , Texture.class);
+        game.getAssetManager().load( "Achieve.png" , Texture.class);
+        game.getAssetManager().load( "settings.png" , Texture.class);
+        game.getAssetManager().finishLoading();
+    }
+
+    public void addListenersActors() {
+        rock.addListener(new InputListener(){
+            public boolean touchDown (InputEvent event, float x, float y, int pointer, int button) {
+                (
+                return true;
+            }
+        });
+    }
 }
